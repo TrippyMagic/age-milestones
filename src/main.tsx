57439062a4265deddb90d81cefcf45774155dd8d
Niccolò Mei <meinicco@gmail.com>
2026-03-26 19:48:45 +0100

    deleted speed insight

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
-//import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./css/index.css";
 import { BirthDateProvider } from "./context/BirthDateContext";
 import { PreferencesProvider } from "./context/PreferencesContext";
